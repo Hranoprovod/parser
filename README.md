519abebbfda84d0fb43c8452511495e383bcb5c6
aquilax <aquilax@gmail.com>
2014-11-21 07:18:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## go-hranoprovod-parser [![Build Status](https://travis-ci.org/Hranoprovod/go-hranoprovod-parser.svg)](https://travis-ci.org/Hranoprovod/go-hranoprovod-parser)
+
+Go hranoprovod file parser
---
+++ errors.go
@@ -0,0 +1,44 @@
+package parser
+
+import (
+	"os"
+)
+
+// process exit codes
+const (
+	exitOk = iota
+	exitErrorIO
+	exitErrorBadSyntax
+	exitErrorConversion
+	exitErrorSingleFoodNotFound
+	exitErrorParsingOptions
+	exitErrorOpeningFile
+	exitErrorGeneral
+)
+
+// BreakingError contains breaking error data
+type BreakingError struct {
+	msg      string
+	exitCode int
+}
+
+// NewBreakingError creates new BreakingError
+func NewBreakingError(msg string, exitCode int) *BreakingError {
+	return &BreakingError{msg, exitCode}
+}
+
+// Error returns the error message
+func (e *BreakingError) Error() string {
+	return e.msg
+}
+
+func handleResult(err error) {
+	if err == nil {
+		os.Exit(exitOk)
+	}
+	println(err.Error())
+	if bErr, ok := err.(*BreakingError); ok {
+		os.Exit(bErr.exitCode)
+	}
+	os.Exit(exitErrorGeneral)
+}
