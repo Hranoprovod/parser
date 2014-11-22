bddb2c6a2b763cf00e15aa3e4bd5a72f40036869
aquilax <evgeniy@wikia-inc.com>
2014-11-22 07:21:02 +0100

    Covered errors

--- errors_test.go
+++ errors_test.go
@@ -1,15 +1,30 @@
 package parser
 
 import (
+	"errors"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func TestBreakingError(t *testing.T) {
-	Convey("Given new IO error", t, func() {
-		err := NewParserErrorIO(nil, "file_name")
-		Convey("Error is of the right type", func() {
+	Convey("Given errors", t, func() {
+		Convey("IO Error works", func() {
+			err := NewParserErrorIO(errors.New("test"), "file_name")
 			So(err.FileName, ShouldEqual, "file_name")
+			So(err.Error(), ShouldEqual, "test")
+		})
+		Convey("Bad Syntax error works", func() {
+			err := NewParserErrorBadSyntax(3, "test line")
+			So(err.Error(), ShouldEqual, "Bad syntax on line 3, \"test line\".")
+			So(err.LineNumber, ShouldEqual, 3)
+			So(err.Line, ShouldEqual, "test line")
+		})
+		Convey("Conversion error works", func() {
+			err := NewParserErrorConversion("bibip", 5, "line string")
+			So(err.Error(), ShouldEqual, "Error converting \"bibip\" to float on line 5 \"line string\".")
+			So(err.Text, ShouldEqual, "bibip")
+			So(err.LineNumber, ShouldEqual, 5)
+			So(err.Line, ShouldEqual, "line string")
 		})
 	})
 }
